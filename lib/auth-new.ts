e5a55853a62233b0352fc6a9787cc4a3476da1b7
mxrcabrera <cabreramxr@gmail.com>
2025-12-02 23:47:18 -0300

    fix: remove Prisma import from auth-new.ts

--- lib/auth-new.ts
+++ lib/auth-new.ts
@@ -1,9 +1,4 @@
 import NextAuth from 'next-auth'
-import { PrismaAdapter } from '@auth/prisma-adapter'
 import { authConfig } from './auth.config'
-import { prisma } from './prisma'
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  adapter: PrismaAdapter(prisma),
-})
+export const { handlers, auth, signIn, signOut } = NextAuth(authConfig)
